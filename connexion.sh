0074edb36539dc8be312ba133aa1b11fea920333
FlavienNoualhaguet <noualhaguet.flavien@gmail.com>
2024-01-25 21:25:11 +0100

    Script bash de connexion au GPU

--- connexion.sh
+++ connexion.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-GPUSER="mds"
-GPU_HOSTNAME="10.222.102.57"
-GPU_HOSTNAME_VIA_PROXY="10.3.5.7"
+# GPUSER="mds"
+# GPU_HOSTNAME="10.222.102.57"
+# GPU_HOSTNAME_VIA_PROXY="10.3.5.7"
+# GPU_PORT=3014
 
-# GPUSER=$USER
-# GPU_HOSTNAME=$HOSTNAME
-# GPU_HOSTNAME_VIA_PROXY=$HOSTNAME
-LOG_FILE=$(basename $0 | cut -d "." -f1).log
+GPUSER=$USER
+GPU_HOSTNAME=$HOSTNAME
+GPU_HOSTNAME_VIA_PROXY=$HOSTNAME
+GPU_PORT=22
+
+LOGFILE=$(basename $0 | cut -d "." -f1).log
 
 # Fonction pour nettoyer les ressources et terminer les processus
 cleanup() {
@@ -22,22 +25,26 @@ cleanup() {
 trap 'cleanup' SIGINT SIGTERM
 
 # Ouverture tunnel un
-tunnel_one="ssh -p 22 $GPUSER@$GPU_HOSTNAME_VIA_PROXY 'jupyter-notebook --no-browser 2>&1 &' > $LOG_FILE 2>&1 &"
-eval $tunnel_one
-
+ssh -p $GPU_PORT $GPUSER@$GPU_HOSTNAME_VIA_PROXY -o ConnectTimeout=5 'jupyter-notebook --no-browser 2>&1 &' > $LOGFILE 2>&1 &
 sleep 5
 
 # Recuperation de l'url depuis le fichier de log
-url=$(grep -E  "^([[:space:]]){1,}http://localhost:[0-9]{4}/*" $LOG_FILE)
+url=$(grep -E  "^([[:space:]]){1,}http://localhost:[0-9]{4}/*" $LOGFILE)
 port=$(echo "$url" | grep -oP 'localhost:\K[0-9]+')
 
-echo URL: $url
-echo Port: $port
+if [[ -z "$url" ]]; then
+    printf "Pas d'url trouvee pour jupyter-notebook.\n"
+    printf "Il y a surement un probleme de connexion ssh\n"
+    exit 1
+fi
 
-tunnel_two="ssh -N -L $port:localhost:8888 $GPUSER@$GPU_HOSTNAME &"
-eval $tunnel_two
+printf "Jupyter-Notebook URL: $url\n"
+printf "Port: $port\n"
 
-if [[ -f "$LOG_FILE" ]]; then rm $LOG_FILE; fi
+# Ouverture tunnel deux
+ssh -N -L $port:localhost:8888 $GPUSER@$GPU_HOSTNAME &
+
+if [[ -f "$LOGFILE" ]]; then rm $LOGFILE; fi
 
 # Lancement de l'url dans le navigateur
 firefox "$url" &
